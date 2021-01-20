7b0579dcbadf75ab8348578048f3d1069f42d535
Sergey190309 <sa6702@gmail.com>
2021-01-20 06:45:57 +0000

    update

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+import App from './compinents/app';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
