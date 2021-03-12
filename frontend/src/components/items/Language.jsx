9e7a77961d8ce8e22cacb1d2079f5a0c1f7dfef9
Sergey <sa6702@gmail.com>
2021-03-12 10:44:26 +0000

    update

--- frontend/src/components/items/Language.jsx
+++ frontend/src/components/items/Language.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import { Flag, Button, Icon } from 'semantic-ui-react'
+import React, { useState } from 'react';
+import { Flag, Button, Icon } from 'semantic-ui-react';
 
 const Language = () => {
+  const [activeLocale, setActiveLocale] = useState('');
+  const [availableLocales, setAvailableLocales] = useState([]);
+
   return (
     <Button basic>
       <Flag name='uk' />
       EN
-      <Icon name='angle down'/>
+      <Icon name='angle down' />
     </Button>
-  )
-}
+  );
+};
 
-export default Language
+export default Language;
