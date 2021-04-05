a6cb2858e7913fca7096a81e17e0e1a0ee3034c0
Sergey <sa6702@gmail.com>
2021-04-05 04:55:09 +0000

    starting db migrations

--- backend/application/contents/local_init_data_contents.py
+++ backend/application/contents/local_init_data_contents.py
@@ -7,9 +7,10 @@ class ContentsConstants():
     def __init__(self):
         # That's initial data for component kinds table:
         self._VIEWS = [
-            {'id_view': 'main', 'description': 'That is main view.'},
+            # {'id_view': 'main', 'description': 'That is main view.'},
             {'id_view': 'hello', 'description': 'That is presentatnion page.'},
-            {'id_view': 'goodbay', 'description': 'That is good bay page.'}]
+            # {'id_view': 'goodbay', 'description': 'That is good bay page.'}
+            ]
 
     @property
     def get_VIEWS(self):
