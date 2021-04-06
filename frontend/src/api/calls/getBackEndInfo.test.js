006a261a88f881e0d53e158ea73d84c372749aa0
Sergey <sa6702@gmail.com>
2021-04-06 12:15:57 +0000

    update

--- frontend/src/api/calls/getBackEndInfo.test.js
+++ frontend/src/api/calls/getBackEndInfo.test.js
@@ -6,23 +6,40 @@ describe('getContents testing', () => {
     one: 'one',
     two: 'two',
     three: 'three',
-  }
+  };
 
   test('success', async () => {
     const mockData = {
       message: 'success message',
       payload: {
         title: 'Title',
-        content: 'Contents'
-      }
-    }
-    const expResults = { ...mockData.payload }
+        content: 'Contents',
+      },
+    };
+    const expResults = { ...mockData.payload };
 
-    mockAxios.get.mockResolvedValueOnce({ data: mockData })
+    mockAxios.get.mockImplementationOnce(() => Promise.resolve({ data: mockData }));
 
-    const result = await getContents(mockKeys)
+    const result = await getContents(mockKeys);
     expect(mockAxios.get).toHaveBeenCalledTimes(1);
+    expect(mockAxios.get).toHaveBeenCalledWith('/contents', { params: mockKeys });
     expect(result).toEqual(expResults);
-    // console.log('result ->', result)
+  });
+
+  test('fail, not found', async () => {
+    const mockData = {
+      response: {
+        data: {
+          message: 'Error message',
+        },
+        status: 404,
+        headers: { header: 'Some header' },
+      },
+      config: { config: 'Some config' },
+    };
+    mockAxios.get.mockImplementationOnce(() => Promise.reject({...mockData}));
+    // expect(getContents(mockKeys)).toThrow()
+    const result = await getContents(mockKeys)
+    console.log(result)
   });
 });
