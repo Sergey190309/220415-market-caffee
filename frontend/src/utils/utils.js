2dbe4495f27941cf80090a059ce28a4991c12887
Sergey <sa6702@gmail.com>
2021-04-09 08:21:03 +0000

    update

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,15 +1,17 @@
 export const idsByIdNum = (id, qnt) => {
+  // The function returns array build as ['id_00', 'id_01', ..., 'id_<qnt>'], qnt should be less then 100 and not negative.
+
+  qnt = qnt < 0 ? 0 : qnt;
+  qnt = qnt > 100 ? 100 : qnt;
+
   const ids = [];
-  let index = 0;
-  while (index < qnt) {
-    let result ;
-    if (index < 10) {
-      result = `_0${index}`
-    } else {
-      result = `_${index}`
-    }
-    console.log(result, 'type -', typeof (result))
-    index++
+
+  // let index = 0;
+  for (let index = 0; index < qnt; index++) {
+    ids.push(index < 10 ? `${id}_0${index}` : `${id}_${index}`);
   }
-  return ids
-}
+  // while (index < qnt) {
+  //   index++;
+  // }
+  return ids;
+};
--- frontend/src/utils/utils.test.js
+++ frontend/src/utils/utils.test.js
@@ -1,17 +1,34 @@
 import { idsByIdNum } from './utils';
 describe('Utils testing', () => {
   test('idsByIdNum testing', () => {
-    const args = {
-      id: 'testingId',
-      qnt: 3,
-    };
+    const id = 'testingId';
+    const qnt = 3
     const expResult = [
-      args.id+'_00',
-      args.id+'_01',
-      args.id+'_02',
+      id+'_00',
+      id+'_01',
+      id+'_02',
     ]
     // console.log(args)
     // console.log(expResult)
-    expect(idsByIdNum({...args})).toEqual(expResult);
+    expect(idsByIdNum(id, qnt)).toEqual(expResult);
+  });
+
+  test('negative qnt should retun empty array', () => {
+    const id = 'testingId';
+    const qnt = -3
+    const expResult = []
+    // console.log(args)
+    // console.log(expResult)
+    expect(idsByIdNum(id, qnt)).toEqual(expResult);
+  });
+
+  test('qnt above 100 shoult retun array length 100', () => {
+    const id = 'testingId';
+    const qnt = 150
+    // const expResult = []
+    // console.log(args)
+    // console.log(expResult)
+    expect(idsByIdNum(id, qnt).length).toEqual(100);
+
   });
 });
