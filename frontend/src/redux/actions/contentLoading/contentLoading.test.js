867d264018e9b7ef4656f0cae9130ec03fb88729
Sergey <sa6702@gmail.com>
2021-06-04 10:49:29 +0000

    update

--- frontend/src/redux/actions/contentLoading/contentLoading.test.js
+++ frontend/src/redux/actions/contentLoading/contentLoading.test.js
@@ -1,12 +1,23 @@
 import { CONTENT_FAIL, CONTENT_START, CONTENT_SUCCESS } from './types';
-import { contentFail, contentStart } from './contentLoading';
+import { contentFail, contentStart, contentSuccess } from './contentLoading';
 
-describe('contentLoading testing', () => {
+describe('contentLoading action testing', () => {
   test('contentStart', () => {
     const expAction = {
       type: CONTENT_START,
     };
     expect(contentStart()).toEqual(expAction);
+  });
+
+  test('contentSuccess', () => {
+    const mockContentInfo = {
+      something: 'something',
+    };
+    const expAction = {
+      type: CONTENT_SUCCESS,
+      payload: mockContentInfo,
+    };
+    expect(contentSuccess(mockContentInfo)).toEqual(expAction);
   });
 
   test('contentFail', () => {
--- frontend/src/redux/reducers/contentLoading/contentLoading.test.js
+++ frontend/src/redux/reducers/contentLoading/contentLoading.test.js
@@ -1,5 +1,16 @@
-describe('contentLoading testing', () => {
-  test('dummy', () => {
+import { CONTENT_START } from '../../actions/contentLoading/types';
+import content, {initialStore } from './contentLoading'
 
+describe('contentLoading reducer testing', () => {
+
+  test('Content start', () => {
+    const testAction = {
+      type: CONTENT_START,
+    };
+    const actStore = { ...initialStore };
+    const expStore = { ...actStore, loading: true };
+
+    expect(actStore).not.toEqual(expStore);
+    expect(content(actStore, testAction)).toEqual(expStore);
   });
 });
