9e8ddbb2e0b71e1c368c66ef5fdf13032de12315
Sergey <sa6702@gmail.com>
2021-07-02 10:51:22 +0000

    update

--- frontend/src/components/page_views/view_elements/ElementSwitcher.test.jsx
+++ frontend/src/components/page_views/view_elements/ElementSwitcher.test.jsx
@@ -1,20 +1,26 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { render, screen } from '../../../testUtils';
 import ElementSwitcher from './ElementSwitcher';
 import { structure } from '../../page_views/landing/Landing.test';
 import ViewHeader from './ViewHeader';
+import ViewFooter from './ViewFooter';
+import ViewVBlock from './ViewVBlock';
+import ViewHBlock from './ViewHBlock';
+import ViewNothing from './ViewNothing';
 
-const MockViewHeader = ({ recordId, viewName, lng }) => {
-  return <div data-testid='ViewHeader' />;
-  // return <div>mockViewHeader</div>;
-};
+const MockViewHeader = ({ recordId, viewName, lng }) => <div data-testid='ViewHeader' />;
+const MockViewFooter = ({ recordId, viewName, lng }) => <div data-testid='ViewFooter' />;
+const MockViewVBlock = ({ recordId, viewName, lng }) => <div data-testid='ViewVBlock' />;
+const MockViewHBlock = ({ recordId, viewName, lng }) => <div data-testid='ViewHBlock' />;
+const MockViewNothing = ({ recordId, viewName, lng }) => (
+  <div data-testid='ViewNothing' />
+);
 
-jest.mock('./ViewHeader', () => ({
-  __esModule: true,
-  // namedExport: jest.fn(),
-  default: jest.fn(),
-}));
-// jest.mock('./ViewHeader');
+jest.mock('./ViewHeader', () => ({ __esModule: true, default: jest.fn() }));
+jest.mock('./ViewFooter', () => ({ __esModule: true, default: jest.fn() }));
+jest.mock('./ViewVBlock', () => ({ __esModule: true, default: jest.fn() }));
+jest.mock('./ViewHBlock', () => ({ __esModule: true, default: jest.fn() }));
+jest.mock('./ViewNothing', () => ({ __esModule: true, default: jest.fn() }));
 // jest.mock('./ViewFooter');
 // jest.mock('./ViewVBlock');
 // jest.mock('./ViewHBlock');
@@ -23,23 +29,62 @@ jest.mock('./ViewHeader', () => ({
 describe('ElementSwitcher testing', () => {
   beforeEach(() => {
     ViewHeader.mockImplementation(MockViewHeader);
+    ViewFooter.mockImplementation(MockViewFooter);
+    ViewVBlock.mockImplementation(MockViewVBlock);
+    ViewHBlock.mockImplementation(MockViewHBlock);
+    ViewNothing.mockImplementation(MockViewNothing);
   });
   const testProps = {
-    structure: { '00': { type: 'header' } },
-    // structure: { ...structure, '05': { type: 'wrongType' } },
+    structure: { ...structure, '05': { type: 'wrongType' } },
     viewName: 'landing',
     lng: 'en',
   };
-  test.skip('it should exist and rendered (snapshot)', () => {
+  test('it should exist and rendered (snapshot)', () => {
     const result = render(<ElementSwitcher {...testProps} />);
     expect(result).toMatchSnapshot();
-    screen.debug();
+    // screen.debug();
   });
   test('call children with appropriate props', () => {
     render(<ElementSwitcher {...testProps} />);
     expect(ViewHeader).toHaveBeenCalledTimes(1);
-    // screen.debug();
-    console.log('ElementSwitcher testing, ViewHeader call ->', ViewHeader.mock.calls[0][0])
-    // expect(ViewHeader).toHaveBeenCalledWith('')
+    expect(ViewFooter).toHaveBeenCalledTimes(1);
+    expect(ViewVBlock).toHaveBeenCalledTimes(2);
+    expect(ViewHBlock).toHaveBeenCalledTimes(1);
+    expect(ViewNothing).toHaveBeenCalledTimes(1);
+
+    // console.log('ElementSwitcher testing, ViewHeader ->', ViewHeader.mock.calls[0][0]);
+    // ----------------------------------------------------------------------------------
+    // recordId serialNumber_type_subType_subComponentQnt
+    // ----------------------------------------------------------------------------------
+    expect(ViewHeader.mock.calls[0][0]).toEqual({
+      recordId: '00_header',
+      viewName: testProps['viewName'],
+      lng: testProps.lng,
+    });
+    expect(ViewVBlock.mock.calls[0][0]).toEqual({
+      recordId: '01_vblock_txt_3',
+      viewName: testProps['viewName'],
+      lng: testProps.lng,
+    });
+    expect(ViewHBlock.mock.calls[0][0]).toEqual({
+      recordId: '02_hblock_pix_2',
+      viewName: testProps['viewName'],
+      lng: testProps.lng,
+    });
+    expect(ViewVBlock.mock.calls[1][0]).toEqual({
+      recordId: '03_vblock_pix_2',
+      viewName: testProps['viewName'],
+      lng: testProps.lng,
+    });
+    expect(ViewFooter.mock.calls[0][0]).toEqual({
+      recordId: '04_footer',
+      viewName: testProps['viewName'],
+      lng: testProps.lng,
+    });
+    expect(ViewNothing.mock.calls[0][0]).toEqual({
+      recordId: '05_wrongType',
+      viewName: testProps['viewName'],
+      lng: testProps.lng,
+    });
   });
 });
