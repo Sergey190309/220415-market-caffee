366136de5fc1cbc8eef7a2e68c84e46e6f771585
Sergey <sa6702@gmail.com>
2021-07-02 14:24:37 +0000

    update

--- frontend/src/components/page_views/view_elements/ViewFooter.test.jsx
+++ frontend/src/components/page_views/view_elements/ViewFooter.test.jsx
@@ -1,5 +1,21 @@
-describe('ViewFooter testing', () => {
-  test('dummy, I do not have enough knowlage to test sagaDispatch', () => {
+import React from 'react';
+import { render, screen } from '../../../testUtils';
 
+import ViewFooter from './ViewFooter'
+
+describe('ViewFooter testing', () => {
+  const testProps = {
+    recordId: 'mockRecordId',
+    viewName: 'mockViewName',
+    lng: 'mockLng',
+    initState: {
+      title: 'mockTitle',
+      content: 'mockContent',
+    },
+  };
+
+  test('it should render (snapshot)', () => {
+    const result = render(<ViewFooter {...testProps} />)
+    expect(result).toMatchSnapshot();
   });
 });
--- frontend/src/components/page_views/view_elements/ViewHeader.test.jsx
+++ frontend/src/components/page_views/view_elements/ViewHeader.test.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
-// import axios from 'axios'
 
-// import { render, screen, waitFor } from '../../../testUtils/modifiedRenderReactTesting';
-import { render, screen, waitFor } from '@testing-library/react';
-
-import '@testing-library/jest-dom';
-// import userEvent from '@testing-library/user-event';
-import renderer from 'react-test-renderer';
+import { render, screen } from '../../../testUtils';
 
 import ViewHeader from './ViewHeader';
 // import { render } from '@testing-library/react';
@@ -14,90 +8,26 @@ import ViewHeader from './ViewHeader';
 // jest.mock('axios');
 
 describe('View header testing', () => {
-  describe('non react components', () => {});
-
-  describe('react components', () => {
-    describe('API activity', () => {
-      const mockTitle = 'This is a title!';
-      const mockContent = 'There is content here';
-      let testProps = {};
-
-      beforeEach(() => {
-        const mockGetValues = jest.fn().mockImplementation(() =>
-          Promise.resolve({
-            title: mockTitle,
-            content: mockContent,
-          })
-        );
-        testProps = {
-          keys: { view_id: 'landing', identity: 'view_heading' },
-          initData: {
-            title: '',
-            content: '',
-          },
-          getValues: mockGetValues,
-        };
-      });
-
-      test.skip('calling API function', async () => {
-        render(<ViewHeader {...testProps} />);
-        await waitFor(() => {
-          const header = screen.getByTestId('header');
-          expect(header).toHaveTextContent(mockTitle);
-
-          // console.log(header)
-          expect(testProps.getValues).toHaveBeenCalledTimes(1);
-          expect(testProps.getValues).toHaveBeenCalledWith(testProps.keys);
-          // const header = screen.getByTestId('header')
-          // expect(header).toHaveTextContent(mockTitle)
-          // expect(header).toHaveClass('ui yellow medium center aligned header', { exact: true })
-          // const content = screen.getByTestId('content')
-          // expect(content).toHaveTextContent(mockContent)
-        });
-      });
-
-      test.skip('Having appropriate text in HTML structure', async () => {
-        render(<ViewHeader {...testProps} />);
-        await waitFor(() => {
-          const header = screen.getByTestId('header');
-          expect(header).toHaveTextContent(mockTitle);
-          expect(header).toHaveClass('ui yellow medium center aligned header', {
-            exact: true,
-          });
-          const content = screen.getByTestId('content');
-          expect(content).toHaveTextContent(mockContent);
-        });
-      });
-    });
-    describe('appeance', () => {
-      const mockTitle = 'This is a title!';
-      const mockContent = 'There is content here';
-      let testProps = {};
-
-      beforeEach(() => {
-        const mockGetValues = jest.fn().mockImplementation(() =>
-          Promise.resolve({
-            title: mockTitle,
-            content: mockContent,
-          })
-        );
-        testProps = {
-          keys: { view_id: 'landing', identity: 'view_heading' },
-          initData: {
-            title: '',
-            content: '',
-          },
-          getValues: mockGetValues,
-        };
-      });
-      test.skip('snapshot', async () => {
-        const result = renderer.create(<ViewHeader {...testProps} />).toJSON();
-        //   const result = await renderer.create(<ViewHeader {...testProps} />)
-        //   const jsonResult = await result.toJSON()
-        expect(result).toMatchSnapshot();
-        // await waitFor(() => {
-        // });
-      });
-    });
+  const testProps = {
+    recordId: 'mockRecordId',
+    viewName: 'mockViewName',
+    lng: 'mockLng',
+    initState: {
+      title: 'mockTitle',
+      content: 'mockContent',
+    },
+  };
+  test('it should render (snapshot)', () => {
+    const result = render(<ViewHeader {...testProps} />)
+    expect(result).toMatchSnapshot();
   });
+
+  test('it should render with correct text)', () => {
+    render(<ViewHeader {...testProps} />)
+    const _title = screen.getByText(testProps['initState']['title'])
+    expect(_title).toBeInTheDocument()
+    const _content = screen.getByText(testProps['initState']['content'])
+    expect(_content).toBeInTheDocument()
+  });
+
 });
