66851d81e44629e1a1c796091ac019bc15985ec3
Sergey <sa6702@gmail.com>
2021-07-13 08:02:56 +0000

    update

--- frontend/src/redux/slices/tech.test.js
+++ frontend/src/redux/slices/tech.test.js
@@ -1,17 +1,20 @@
-import store from '../store'
-import { initialState, startInitLoading } from './tech'
+import { techAxiosClient } from '../../api/apiClient';
+import store from '../store';
+import { initialState, startInitLoading } from './tech';
 describe('Tech slice testing', () => {
-  const mockPayload = {}
-  // let state
+  const mockPayload = {};
+  const mockTechInResolve = 'mockTechToken';
   beforeEach(() => {
-    // state = store.getState().tech
-    // expect(state).toEqual(initialState);
-  })
-  test('startInitLoading', () => {
-    let state = store.getState().tech
-    store.dispatch(startInitLoading())
-    state = store.getState().tech
+    jest.resetAllMocks();
+  });
+  test('startInitLoading', async () => {
+    techAxiosClient.post.mockImplementation(() =>
+      Promise.resolve({ data: mockTechInResolve })
+    );
+    let state = store.getState().tech;
+    store.dispatch(startInitLoading());
+    state = await store.getState().tech;
     // expect(state.loading).toBeTruthy();
-    console.log('startInitLoading, state ->', state)
+    console.log('startInitLoading, state ->', state);
   });
 });
