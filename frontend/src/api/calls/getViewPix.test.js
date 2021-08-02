81f71c055c38aa2f788b8be2d7e89735e43da7db
Sergey190309 <sa6702@gmail.com>
2021-08-02 10:02:58 +0000

    update

---
+++ frontend/src/api/calls/getViewPix.test.js
@@ -0,0 +1,5 @@
+describe('getViewPix testing', () => {
+  test('dummy', () => {
+
+  });
+});
---
+++ frontend/src/components/nav_items/Logo.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Image} from 'semantic-ui-react';
+// import logo from '../../../assets/images/logo.png'
+
+const Logo = ({ color, inverted }) => {
+  const src = '../../../assets/images/logo.png';
+
+  // console.log(color)
+  return (
+      <Image
+        src={src}
+        // inverted
+        size='tiny'
+        // color={color}
+      />
+  );
+};
+
+export default Logo;
