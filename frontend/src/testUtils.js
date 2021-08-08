3ad615a903aa1b25426c5a5b7412289a5425983c
Sergey <sa6702@gmail.com>
2021-08-08 13:33:12 +0000

    update

--- frontend/src/testUtils.js
+++ frontend/src/testUtils.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { combineReducers } from 'redux';
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { combineReducers, createStore } from 'redux'
 
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { runSaga } from 'redux-saga';
+import { Provider } from 'react-redux'
+import { runSaga } from 'redux-saga'
 
-import { render } from '@testing-library/react';
+import { render } from '@testing-library/react'
 
 // import alerts from '../redux/reducers/alert'
 // import device from '../redux/reducers/device'
 // import logIn from '../redux/reducers/auth'
 // import lng from '../redux/reducers/lng'
 
-import rootReducer from './redux/slices';
+import rootReducer from './redux/slices'
 // import reducer from './redux/slices';
 // ===================================================
 // The block about rendering connected components.
@@ -23,17 +22,17 @@ const connectedRender = (
   { initialState, store = createStore(rootReducer, initialState), ...renderOptions } = {}
 ) => {
   const Wrapper = ({ children }) => {
-    return <Provider store={store}>{children}</Provider>;
-  };
-  return render(ui, { wrapper: Wrapper, ...renderOptions });
-};
+    return <Provider store={store}>{children}</Provider>
+  }
+  return render(ui, { wrapper: Wrapper, ...renderOptions })
+}
 
 const linkedRender = (ui, { ...renderOptions } = {}) => {
   const Wrapper = ({ children }) => {
-    return <BrowserRouter>{children}</BrowserRouter>;
-  };
-  return render(ui, { wrapper: Wrapper, ...renderOptions });
-};
+    return <BrowserRouter>{children}</BrowserRouter>
+  }
+  return render(ui, { wrapper: Wrapper, ...renderOptions })
+}
 
 const connectedLinkedRender = (
   ui,
@@ -46,15 +45,15 @@ const connectedLinkedRender = (
       <Provider store={store}>
         <BrowserRouter>{children}</BrowserRouter>
       </Provider>
-    );
-  };
-  return render(ui, { wrapper: Wrapper, ...renderOptions });
-};
+    )
+  }
+  return render(ui, { wrapper: Wrapper, ...renderOptions })
+}
 
 // const linkedRender = (ui) => {}
 
-export * from '@testing-library/react';
-export { connectedRender, linkedRender, connectedLinkedRender };
+export * from '@testing-library/react'
+export { connectedRender, linkedRender, connectedLinkedRender }
 // ===================================================
 
 // ===================================================
@@ -62,25 +61,25 @@ export { connectedRender, linkedRender, connectedLinkedRender };
 // ---------------------------------------------------
 
 export const recordSaga = async (saga, initialAction) => {
-  const dispatched = [];
+  const dispatched = []
   // console.log(saga)
 
   await runSaga(
     {
-      dispatch: action => dispatched.push(action),
+      dispatch: action => dispatched.push(action)
     },
     saga,
     initialAction
-  ).done;
+  ).done
 
-  return dispatched;
-};
+  return dispatched
+}
 
 // ===================================================
 /**
  * Return store based on live reducers from slicers
  */
 export const createTestStore = () => {
-  const store = createStore(combineReducers({ ...rootReducer }));
-  return store;
-};
+  const store = createStore(combineReducers({ ...rootReducer }))
+  return store
+}
