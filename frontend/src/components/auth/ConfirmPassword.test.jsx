2d76aeb5e2c95f79d7be7fbaf912318a70099fb2
Sergey <sa6702@gmail.com>
2021-08-19 06:31:08 +0000

    update

--- frontend/src/components/auth/ConfirmPassword.test.jsx
+++ frontend/src/components/auth/ConfirmPassword.test.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-describe('ConfirmPassword', () => {
-=======
 describe('ConfirmPassword testing', () => {
->>>>>>> 9a8ca37ed90d37570174338b4c29fff89bf59302
   test('dummy', () => {
 
   })
